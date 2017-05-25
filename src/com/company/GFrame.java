1bb621ce5f28ffda38a045a802cb34dcd5a79852
sknfshka <ya.peacemaker@yandex.ru>
2017-05-25 20:08:30 +0300

    commit

--- src/com/company/GFrame.java
+++ src/com/company/GFrame.java
@@ -2,64 +2,28 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class GFrame extends JFrame{
     private GPanel gPanel;
-    JMenuBar jMenuBar;
-    JMenu gameMenu, optionsMenu;
-    JMenuItem newGameItem, exitItem, imageModeItem, signModeItem;
+    private JMenuBar jMenuBar;
+    private JMenu gameMenu, optionsMenu, helpMenu;
+    private JMenuItem newGameItem, exitItem, imageModeItem, signModeItem, aboutProgramm;
 
     public  GFrame() {
         gPanel = new GPanel(this);
 
         add(gPanel);
 
+        UIManager.put("OptionPane.yesButtonText", "Да");
+        UIManager.put("OptionPane.noButtonText", "Нет");
+        UIManager.put("OptionPane.cancelButtonText", "Отмена");
+        UIManager.put("OptionPane.okButtonText", "Готово");
+
         jMenuBar = new JMenuBar();
 
-        gameMenu = new JMenu("Игра");
-        optionsMenu = new JMenu("Опции");
+        createJMenus();
 
-        jMenuBar.add(gameMenu);
-        jMenuBar.add(optionsMenu);
-
-        newGameItem = new JMenuItem("Новая");
-        gameMenu.add(newGameItem);
-        exitItem = new JMenuItem("Выход");
-        gameMenu.add(exitItem);
-
-        exitItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
-
-        imageModeItem = new JRadioButtonMenuItem("Изображение", true);
-        optionsMenu.add(imageModeItem);
-        signModeItem = new JRadioButtonMenuItem("Цифры");
-        optionsMenu.add(signModeItem);
-        imageModeItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                signModeItem.setSelected(false);
-                imageModeItem.setSelected(true);
-                gPanel.switchMode();
-            }
-        });
-        signModeItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                imageModeItem.setSelected(false);
-                signModeItem.setSelected(true);
-                gPanel.switchMode();
-            }
-        });
-
-        newGameItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                gPanel.startNewGame();
-                setRadioEnabled(true);
-            }
-        });
+        createJMenuItems();
 
         setJMenuBar(jMenuBar);
 
@@ -71,9 +35,59 @@ public class GFrame extends JFrame{
         setResizable(false);
     }
 
-    void setRadioEnabled(boolean enabled) {
+    private void createJMenus() {
+        gameMenu = new JMenu("Игра");
+        optionsMenu = new JMenu("Опции");
+        helpMenu = new JMenu("Помощь");
 
+        jMenuBar.add(gameMenu);
+        jMenuBar.add(optionsMenu);
+        jMenuBar.add(helpMenu);
+    }
+
+    private void createJMenuItems() {
+        newGameItem = new JMenuItem("Новая");
+        gameMenu.add(newGameItem);
+
+        exitItem = new JMenuItem("Выход");
+        gameMenu.add(exitItem);
+
+        exitItem.addActionListener(e -> System.exit(0));
+
+        imageModeItem = new JRadioButtonMenuItem("Изображение", true);
+        optionsMenu.add(imageModeItem);
+
+        signModeItem = new JRadioButtonMenuItem("Цифры");
+        optionsMenu.add(signModeItem);
+
+        imageModeItem.addActionListener(e -> {
+            signModeItem.setSelected(false);
+            imageModeItem.setSelected(true);
+            gPanel.switchMode();
+        });
+        signModeItem.addActionListener(e -> {
+            imageModeItem.setSelected(false);
+            signModeItem.setSelected(true);
+            gPanel.switchMode();
+        });
+
+        newGameItem.addActionListener(e -> {
+            gPanel.startNewGame();
+            setRadioEnabled(true);
+        });
+
+        aboutProgramm = new JMenuItem("О программе");
+        helpMenu.add(aboutProgramm);
+
+        aboutProgramm.addActionListener(e -> showProgrammInfo());
+    }
+
+    void setRadioEnabled(boolean enabled) {
         imageModeItem.setEnabled(enabled);
         signModeItem.setEnabled(enabled);
+    }
+
+    private void showProgrammInfo() {
+        JOptionPane.showMessageDialog(this, "Программа создана в рамках курса \"Технологии программирования\"");
     }
 }
--- src/com/company/GPanel.java
+++ src/com/company/GPanel.java
@@ -12,25 +12,24 @@ import java.io.IOException;
 import java.util.Random;
 
 public class GPanel extends JPanel{
-    final static int SEEDS = 40;
+    final static int SEEDS = 40;    // используется для перемешивания поля
 
-    private int gameMode = 1; // 1 - pic, 2 - sign
-    private String imgPath = "src.jpg";
-    private boolean firstClick;
-    private boolean isGameOver;
-    private BufferedImage img;
+    private int gameMode = 1; // 1 - изображение, 2 - символы
+    private String imgPath = "src.jpg"; // путь до исходного изображения
+    private boolean firstClick; // для перемешивания поля по первому нажатию
+    private BufferedImage img;  // сохраненное исходное изображение
     private Random random = new Random();
 
-    private GCell[][] gCells = new GCell[4][4];
-    private Point freeSpaceCoordinate = new Point();
-    private GFrame gFrame;
+    private GCell[][] gCells = new GCell[4][4]; // клетки поля
+    private Point freeSpaceCoordinate = new Point(); // координаты свободной клетки
+    private GFrame gFrame;  // ссылка на frame в котором вызвано поле (для победы, блокировки переключения mode на frame)
 
+    /* Клетка поля */
     private class GCell{
         static final int CELL_SIZE = 160;
 
         int number;
         BufferedImage image;
-        // кусок картинки
         public GCell(){
 
         }
@@ -43,10 +42,6 @@ public class GPanel extends JPanel{
         public GCell(GCell gCell) {
             this.number = gCell.number;
             this.image = gCell.image;
-        }
-
-        public BufferedImage getImage() {
-            return image;
         }
     }
 
@@ -62,17 +57,14 @@ public class GPanel extends JPanel{
 
                 @Override
                 public void mousePressed(MouseEvent e) {
+                    /* Перемещение клеток поля, блокирование переключателей на фрейме */
                     if (firstClick) {
                         mixCells();
                         gFrame.setRadioEnabled(false);
                         firstClick = false;
                     }
+                    /* Нажатие на клетку */
                     else {
-                        if (isGameOver) {
-                            initCells();
-                            return;
-                        }
-
                         mousePressAction(e.getPoint());
                         checkWin();
                     }
@@ -83,7 +75,7 @@ public class GPanel extends JPanel{
             e.printStackTrace();
         }
     }
-
+    /* Действие на нажатие */
     private void mousePressAction(Point point) {
         int x = (int) point.getX(), y = (int) point.getY(),
                 fieldSize = getGPanelSize();
@@ -97,31 +89,28 @@ public class GPanel extends JPanel{
 
         moveToFreeSpace(targetPoint);
     }
-
-
+    /* Проверка на окончание игры, начало новой */
     private void checkWin() {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4 ; j++) {
-                if (gCells[i][j].number != j*4 + i + 1) {
-                    isGameOver = false;
+                if (gCells[i][j].number != j*4 + i + 1)
                     return;
-                }
             }
         }
 
-        isGameOver = true;
-        System.out.println("u win!");
+        JOptionPane.showMessageDialog(gFrame, "Вы выиграли, для начала новой игры нажмите \"Готово\"");
+        startNewGame();
     }
-
+    /* Отрезать от src изображения начиная с точки point */
     private BufferedImage cropImage(BufferedImage src, Point point) {
         return src.getSubimage((int) point.getX(), (int) point.getY(), GCell.CELL_SIZE, GCell.CELL_SIZE);
     }
-
+    /* Начало новой игры */
     public void startNewGame() {
         initCells();
         repaint();
     }
-
+    /* Инициализация клеток */
     private void initCells() {
         freeSpaceCoordinate.setLocation(3, 3);
 
@@ -133,8 +122,7 @@ public class GPanel extends JPanel{
 
         firstClick = true;
     }
-
-    /* перемешать клетки на поле*/
+    /* Перемешать клетки на поле*/
     private void mixCells() {
         int firstRandomNumber, secondRandomNumber;
         for (int i = 0; i < SEEDS; i++) {
@@ -142,10 +130,9 @@ public class GPanel extends JPanel{
             secondRandomNumber = random.nextInt(15);
             Point point1 = new Point(firstRandomNumber / 4, firstRandomNumber % 4);
             Point point2 = new Point(secondRandomNumber / 4, secondRandomNumber % 4);
-            switchPoints(point1, point2);
+            swapPoints(point1, point2);
         }
 
-        isGameOver = false;
         repaint();
     }
 
@@ -166,16 +153,16 @@ public class GPanel extends JPanel{
         }
 
     }
-
+    /* Отрисовка панели как изображения */
     private void paintImagePanel(Graphics g) {
         for(int i = 0; i < 4; i++) {
             for(int j = 0; j < 4; j++) {
                 if (!(freeSpaceCoordinate.getX() == i && freeSpaceCoordinate.getY() == j))
-                    g.drawImage(gCells[i][j].getImage(), i * GCell.CELL_SIZE, j * GCell.CELL_SIZE, this);
+                    g.drawImage(gCells[i][j].image, i * GCell.CELL_SIZE, j * GCell.CELL_SIZE, this);
             }
         }
     }
-
+    /* Отрисовка панели как символы */
     private void paintSignPanel(Graphics2D g2) {
         for (int i = 0; i < 4; i++) {
 
@@ -190,7 +177,7 @@ public class GPanel extends JPanel{
             }
         }
     }
-
+    /* Печать символа по центру клетки*/
     private void paintSymbol(int x, int y, String text,Graphics2D g2){
         int w = GCell.CELL_SIZE * --x;
         int h = GCell.CELL_SIZE * --y;
@@ -206,11 +193,11 @@ public class GPanel extends JPanel{
 
         g2.drawString(text, w + textX, h + textY);
     }
-
+    /* Размер поля */
     public int getGPanelSize() {
         return GCell.CELL_SIZE * 4;
     }
-
+    /* Изменить mod */
     public void switchMode() {
         switch (gameMode) {
             case 1: {
@@ -224,22 +211,22 @@ public class GPanel extends JPanel{
         }
         repaint();
     }
-
+    /* Может ли клетка быть перемещена на свободную позицию */
     private boolean canBeMoved(Point point) {
         int xDif = (int)point.getX() - (int)freeSpaceCoordinate.getX(), yDif = (int)point.getY() - (int)freeSpaceCoordinate.getY();
         return ((Math.abs(xDif) == 1) && ( yDif == 0)) || ((Math.abs(yDif) == 1) && ( xDif == 0));
     }
-
+    /* Перемещение на свободную клетку */
     private void moveToFreeSpace(Point point) {
         if(canBeMoved(point)) {
-            switchPoints(point, freeSpaceCoordinate);
+            swapPoints(point, freeSpaceCoordinate);
 
             freeSpaceCoordinate = point;
         }
         repaint();
     }
-
-    private void switchPoints(Point point1, Point point2) {
+    /* Перестановка клеток */
+    private void swapPoints(Point point1, Point point2) {
         int x1 = (int) point1.getX(), y1 = (int) point1.getY(),
                 x2 = (int) point2.getX(), y2 = (int) point2.getY();
 
